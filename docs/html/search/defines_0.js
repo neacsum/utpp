d44469f32c990fda21e4a481c90872945a622098
Mircea Neacsu <mircea@neacsu.net>
2018-01-10 12:35:26 -0500

    Updated documentation

--- docs/html/search/defines_0.js
+++ docs/html/search/defines_0.js
@@ -4,7 +4,6 @@ var searchData=
   ['check_5farray2d_5fclose',['CHECK_ARRAY2D_CLOSE',['../check__macros_8h.html#aa221a99c264579731650cb05888f4819',1,'check_macros.h']]],
   ['check_5farray_5fclose',['CHECK_ARRAY_CLOSE',['../check__macros_8h.html#ae6d7b175b274884e9c9fa474c697774f',1,'check_macros.h']]],
   ['check_5farray_5fequal',['CHECK_ARRAY_EQUAL',['../check__macros_8h.html#ab805d258626678b2e84a42f51436ecd0',1,'check_macros.h']]],
-  ['check_5fassert',['CHECK_ASSERT',['../check__macros_8h.html#aa0b5079e311f12736a3c5426380da7b5',1,'check_macros.h']]],
   ['check_5fclose',['CHECK_CLOSE',['../check__macros_8h.html#a4e1bd8b7b16e3e16c07270b99cc4de75',1,'check_macros.h']]],
   ['check_5fequal',['CHECK_EQUAL',['../check__macros_8h.html#ad8fa79f5d491cc01af302832785e90fe',1,'check_macros.h']]],
   ['check_5fex',['CHECK_EX',['../check__macros_8h.html#a1f07f244613de24d8825b75e135bd24a',1,'check_macros.h']]],
--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['add',['Add',['../class_unit_test_1_1_suites_list.html#a1b6c210dd25c0853293e3f1de5b7cff6',1,'UnitTest::SuitesList::Add()'],['../class_unit_test_1_1_test_suite.html#a4305f54e3877782a022c05fef3de123f',1,'UnitTest::TestSuite::Add()']]],
-  ['areclose',['AreClose',['../checks_8h.html#a7fa43c88ce656da25067ea8663fc5d2f',1,'UnitTest']]]
+  ['add',['Add',['../class_unit_test_1_1_suites_list.html#a1b6c210dd25c0853293e3f1de5b7cff6',1,'UnitTest::SuitesList::Add()'],['../class_unit_test_1_1_test_suite.html#a4305f54e3877782a022c05fef3de123f',1,'UnitTest::TestSuite::Add()']]]
 ];
