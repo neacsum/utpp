883d4e08a426e5ab75217a81db5ea96180a6a82b
Mircea Neacsu <mircea@neacsu.net>
2020-06-16 07:37:33 -0400

    Check if VS environment is already in place.

--- setup.bat
+++ setup.bat
@@ -1,14 +1,18 @@
 @echo off
 rem
-rem setup environment for Visual Stuio 2017 and later
+rem Setup environment for MSBuild 2017 and later
 rem
 
+if not "%VSCMD_VER%"=="" (
+  echo MSBuild environment is already set
+  goto :EOF
+) 
 rem VS2017U2 contains vswhere.exe
 if "%VSWHERE%"=="" set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 
 for /f "usebackq tokens=*" %%i in (`"%VSWHERE%" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
   set VSInstallDir=%%i
 )
-echo Visual studio installation folder is: %VSInstallDir%
+rem echo Visual studio installation folder is: %VSInstallDir%
 rem "%VSInstallDir%\common7\tools\vsdevcmd.bat"
 "%VSInstallDir%\common7\tools\vsmsbuildcmd.bat"
