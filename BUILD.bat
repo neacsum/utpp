8dcaad383efb2568e89d1524f686ee683fa9b94f
Mircea Neacsu <mircea@neacsu.net>
2021-06-29 21:27:04 -0400

    build - can do clean and rebuild

--- BUILD.bat
+++ BUILD.bat
@@ -3,7 +3,10 @@ rem
 rem  Master build file. Execute this script to create all output libraries
 rem  Environment variable DEV_ROOT points to the root of development tree.
 rem
+rem  Syntax:
+rem     BUILD [clean | rebuild]
 
 call mklinks.bat
 call setup.bat
-msbuild build.proj
+
+if "%~1"=="" (msbuild build.proj) else (msbuild build.proj /t:%1)
